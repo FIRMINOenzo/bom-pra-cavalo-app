408a7b54c2adcbb623fd1d6a9a8a5ace7d370a6d
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-09-09 10:33:00 -0300

    chore: added .env file

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,21 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import WorkoutCard from "./src/components/WorkoutCard";
+import * as AuthSession from "expo-auth-session";
 
 export default function App() {
     return (
-        <>
-            <View className="h-16 w-full bg-black shadow-lg"></View>
-            <View className="flex-1 bg-gray-900">
-                <View className="flex w-full flex-row justify-between h-10  px-4">
-                    <Text className="text-3xl text-white font-bold">A</Text>
-                    <TouchableOpacity>
-                        <Text>Adicionar</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <StatusBar style="auto" translucent={true} />
+        <View className="flex-1 bg-gray-900">
+            <View className="flex w-full flex-row justify-between h-10  px-4">
+                <Text className="text-3xl text-white font-bold">A</Text>
+                <TouchableOpacity>
+                    <Text>Adicionar</Text>
+                </TouchableOpacity>
             </View>
-        </>
+        </View>
     );
 }
